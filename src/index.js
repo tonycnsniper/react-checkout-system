265eee0f82ffc68379d584df339b8546fa4d0e39
Tony Chao <tony_sniper@163.com>
2023-12-22 22:43:19 +1300

    cj - init slices and reducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
-render(<h2>Hello</h2>, document.getElementById('root'));
+import App from './App'
+import rootReducer from './slices'
+
+import './index.css'
+
+const store = configureStore({ reducer: rootReducer });
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
